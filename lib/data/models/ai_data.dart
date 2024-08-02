cc230d775d7111f27872402275f339dd1fdc4280
v2n03 <shgudwns1515@naver.com>
2024-08-02 16:41:16 +0900

    ai-data주고 받기 구현 with token

--- lib/data/models/ai_data.dart
+++ lib/data/models/ai_data.dart
@@ -10,11 +10,11 @@ class AiData with _$AiData {
   const factory AiData({
     required List<String> cosNames,
     required List<String> cosPaths,
+    required List<SkinType> expertSkin,
     required List<String> nutrNames,
     required List<String> nutrPaths,
     required String simpleSkin,
-    required List<SkinType> expertSkin,
-    required String resultPath,
+    required String resultImage,
   }) = _AiData;
 
   factory AiData.fromJson(Map<String, dynamic> json) => _$AiDataFromJson(json);
