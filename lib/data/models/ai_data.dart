792250ad04007e3e187051b87213cc082d8e7209
v2n03 <shgudwns1515@naver.com>
2024-08-06 15:44:35 +0900

    fixing

--- lib/data/models/ai_data.dart
+++ lib/data/models/ai_data.dart
@@ -5,47 +5,6 @@ part 'ai_data.g.dart';
 
 enum SkinType { acne, wrinkle, atophy }
 
-extension SkinTypeExtension on SkinType {
-  String toJson() {
-    return toString().split('.').last.toUpperCase();
-  }
-
-  static SkinType fromJson(String json) {
-    switch (json) {
-      case 'ACNE':
-        return SkinType.acne;
-      case 'WRINKLE':
-        return SkinType.wrinkle;
-      case 'ATOPHY':
-        return SkinType.atophy;
-      default:
-        throw ArgumentError('Unknown skin type: $json');
-    }
-  }
-}
-
-class SkinTypeConverter implements JsonConverter<SkinType, String> {
-  const SkinTypeConverter();
-
-  @override
-  SkinType fromJson(String json) => SkinTypeExtension.fromJson(json);
-
-  @override
-  String toJson(SkinType object) => object.toJson();
-}
-
-class SkinTypeListConverter implements JsonConverter<List<SkinType>, List<String>> {
-  const SkinTypeListConverter();
-
-  @override
-  List<SkinType> fromJson(List<String> json) =>
-      json.map((e) => SkinTypeExtension.fromJson(e)).toList();
-
-  @override
-  List<String> toJson(List<SkinType> object) =>
-      object.map((e) => e.toJson()).toList();
-}
-
 @freezed
 class AiData with _$AiData {
   const factory AiData({
@@ -54,7 +13,7 @@ class AiData with _$AiData {
     required List<String> nutrNames,
     required List<String> nutrPaths,
     required String simpleSkin,
-    @SkinTypeListConverter() List<SkinType>? expertSkin,
+    List<SkinType>? expertSkin,
     required String resultImage,
   }) = _AiData;
 
